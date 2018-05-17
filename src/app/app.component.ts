71b1aaffaa86da15b161b84b23d4d7e3f5d0156c
Clive Gordon <clivejay492@gmail.com>
2018-05-17 13:00:50 +0100

    Add menu tree functionality, user menu styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -77,6 +77,127 @@ export class AppComponent {
     this.menuState = this.menuState === 'mobileOut' ? 'mobileIn' : 'mobileOut';
   }
 
+    //Menu tree
+    title: String;
+    menuList: any;
+    selected: any;
+
+    constructor() {
+        this.title = 'Collapsible menu - Angular 2';
+        this.menuList = [
+            {
+                'name': 'Training and assessment',
+                'subMenu1': ['Study support'],
+                'subsubMenu1': [
+                    'Method of study',
+                    'Types of study and revision support',
+                    'Study support search',
+                    'AAT Accounting Qualifications (AQ2016) update',
+                    'Short qualifications 2016 updates',
+                    'Excel e-learning',
+                    'Anti-Money Laundering and Anti-Bribery and Corruption e-learning'
+                ],
+                'subMenu2': ['Assessments'],
+                'subsubMenu2': [
+                    'CBA five steps',
+                    'Your assessment results',
+                    'Synoptic assessment windows and results',
+                    'SecureAssess information and support',
+                    'CBP - web access',
+                    'General assessment FAQs',
+                    'Find an assessment venue',
+                    'Statements of Achievement',
+                    'Enquiries and appeals'
+                ]
+            },
+            {
+                'name': 'Professional zone',
+                'subMenu1': ['CPD zone'],
+                'subsubMenu1': [
+                    'Get started with CPD',
+                    'Our CPD policy',
+                    'CPD monitoring',
+                    'CPD resources',
+                    'Online CPD record',
+                    'CPD events and courses',
+                ],
+                'subMenu2': ['Professional standards'],
+                'subsubMenu2': [
+                    'Professional standards',
+                    'Fit and proper requirements',
+                    'Professional ethics'
+                ]
+            },
+            {
+                'name': 'My Career',
+                'subMenu1': ['Students'],
+                'subsubMenu1': [
+                    'Career support',
+                    'Going on to university',
+                    'Become an AAT licensed member',
+                    'Work and study internationally'
+                ]
+                //NO items in this menu tree
+                // 'subMenu2': [''],
+                // 'subsubMenu2': [
+                //   ''
+                // ]
+            },
+            {
+                'name': 'My Membership',
+                'subMenu1': ['Student members'],
+                'subsubMenu1': [
+                    'Benefits of student membership',
+                    'Benefits of bookkeeping membership',
+                    'NUS extra card',
+                    'Your student member toolkit',
+                    'Pay outstanding fees',
+                    'Benefits of full membership',
+                    'Apply for full membership',
+                    'Work experience guidance',
+                    'AAT Rewards',
+                    'Fees information'
+                ]
+                //NO items in this menu tree
+                // 'subMenu2': [''],
+                // 'subsubMenu2': [
+                //   '',
+                // ]
+            },
+            {
+                'name': 'News and events',
+                'subMenu1': ['News'],
+                'subsubMenu1': [
+                    'AAT Comment',
+                    'AAT Weekly',
+                    'Engage'
+                ],
+                'subMenu2': ['Events'],
+                'subsubMenu2': [
+                    'Types of events',
+                    'Events search',
+                    'Event terms and conditions'
+                ]
+            }
+        ];
+    }
+
+    select(item) {
+        this.selected = (this.selected === item ? null : item);
+    }
+
+    isActive(item) {
+        return this.selected === item;
+    }
+
+    select1(item1) {
+        this.selected = (this.selected === item1 ? null : item1);
+    }
+
+    isActiveChild(item1) {
+        return this.selected === item1;
+    }
+
   // state: string = 'fadein';
   // // items = ['item1', 'item2', 'item3'];
   // items = new Array();
